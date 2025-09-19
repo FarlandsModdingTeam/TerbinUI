a1fcc4b932b43f2374604be05d9aed1cc2f8f50d
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2025-09-19 06:41:53 +0200

    electronNet + Blazor "instalado"

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+using TerbinUI.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
+*/
+using ElectronNET.API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuración de servicios
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+//builder.WebHost.UseUrls("http://localhost:8000");
+
+var app = builder.Build();
+
+// Middleware
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("\\obj\\Host\\splashscreen");
+
+// 🚀 Aquí está la clave: integración con Electron
+if (HybridSupport.IsElectronActive)
+{
+    Task.Run(async () =>
+    {
+        var window = await Electron.WindowManager.CreateWindowAsync();
+        window.OnClosed += () => Environment.Exit(0);
+    });
+}
+
+app.Run();
